27d3ffbec10048f8da62ee6c891f9b7632a113fb
Abhiti Prabahar <abhiti.prabahar@gmail.com>
2020-04-22 18:25:20 -0400

    Fixes tests

--- src/Events/Authentication/__tests__/CreatedAccount.test.ts
+++ src/Events/Authentication/__tests__/CreatedAccount.test.ts
@@ -8,15 +8,20 @@ describe("createdAccount", () => {
       contextModule: ContextModule.header,
       intent: AuthIntent.signup,
       service: "apple",
+      user_id: "foo",
     })
 
     expect(args).toEqual({
       action: "createdAccount",
       auth_redirect: "https://artsy.net/artist/andy-warhol",
       context_module: "header",
+      modal_copy: undefined,
       intent: "signup",
+      onboarding: false,
       service: "apple",
       trigger: "click",
+      type: "signup",
+      user_id: "foo",
     })
   })
 
@@ -24,18 +29,25 @@ describe("createdAccount", () => {
     const args = createdAccount({
       authRedirect: "https://artsy.net/artist/andy-warhol",
       contextModule: ContextModule.popUpModal,
+      copy: "Sign up to follow artists",
       intent: AuthIntent.viewEditorial,
+      onboarding: true,
       service: "email",
       triggerSeconds: 2,
+      user_id: "foo",
     })
 
     expect(args).toEqual({
       action: "createdAccount",
       auth_redirect: "https://artsy.net/artist/andy-warhol",
       context_module: "popUpModal",
+      modal_copy: "Sign up to follow artists",
       intent: "viewEditorial",
+      onboarding: true,
       service: "email",
       trigger: "timed",
+      type: "signup",
+      user_id: "foo",
     })
   })
 })
--- src/Events/Authentication/__tests__/SuccessfullyLoggedIn.test.ts
+++ src/Events/Authentication/__tests__/SuccessfullyLoggedIn.test.ts
@@ -8,6 +8,7 @@ describe("successfullyLoggedIn", () => {
       contextModule: ContextModule.header,
       intent: AuthIntent.signup,
       service: "apple",
+      user_id: "foo",
     })
 
     expect(args).toEqual({
@@ -15,8 +16,11 @@ describe("successfullyLoggedIn", () => {
       auth_redirect: "https://artsy.net/artist/andy-warhol",
       context_module: "header",
       intent: "signup",
+      modal_copy: undefined,
       service: "apple",
       trigger: "click",
+      type: "login",
+      user_id: "foo",
     })
   })
 
@@ -24,9 +28,11 @@ describe("successfullyLoggedIn", () => {
     const args = successfullyLoggedIn({
       authRedirect: "https://artsy.net/artist/andy-warhol",
       contextModule: ContextModule.popUpModal,
+      copy: "Sign up to follow artists",
       intent: AuthIntent.viewEditorial,
       service: "email",
       triggerSeconds: 2,
+      user_id: "foo",
     })
 
     expect(args).toEqual({
@@ -34,8 +40,11 @@ describe("successfullyLoggedIn", () => {
       auth_redirect: "https://artsy.net/artist/andy-warhol",
       context_module: "popUpModal",
       intent: "viewEditorial",
+      modal_copy: "Sign up to follow artists",
       service: "email",
       trigger: "timed",
+      type: "login",
+      user_id: "foo",
     })
   })
 })
