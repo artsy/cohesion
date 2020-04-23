04281e90f21d0e3a72824356b9158337703a83af
Eve Essex <eve.essex@gmail.com>
2020-04-23 16:37:54 -0400

    add export for helper

--- src/Events/index.ts
+++ src/Events/index.ts
@@ -1,3 +1,4 @@
 export * from "./Authentication/AuthImpression"
 export * from "./Authentication/CreatedAccount"
 export * from "./Authentication/SuccessfullyLoggedIn"
+export * from "./Authentication/ResetYourPassword"
