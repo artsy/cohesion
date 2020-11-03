6d41b89e906d9ca7e957c5d3c53726b304f0a394
Mike Hromchak <mike@artsymail.com>
2020-11-03 17:45:21 -0500

    Track sale_screen_load_complete

--- src/Schema/Events/System.ts
+++ src/Schema/Events/System.ts
@@ -27,3 +27,31 @@ export interface TimeOnPage {
   context_page_owner_id?: string
   context_page_owner_slug?: string
 }
+
+/**
+ * A user loads a sale screen on the iOS app.
+ *
+ * This schema describes events sent to Segment from [[saleScreenLoadComplete]]
+ *
+ *  @example
+ *  ```
+ *  {
+ *    action: "saleScreenLoadComplete",
+ *    context_page_owner_type: "auction",
+ *    context_page_owner_id: "5f841d4044f91e000fd0acc4",
+ *    context_page_owner_slug: "finarte-modern-and-contemporary-art-8",
+ *    jump_to_lai_interface: true,
+ *    load_time_ms: 2000,
+ *    number_of_lots: 276
+ *  }
+ * ```
+ */
+export interface SaleScreenLoadComplete {
+  action: ActionType.saleScreenLoadComplete
+  context_page_owner_type: PageOwnerType
+  context_page_owner_id?: string
+  context_page_owner_slug?: string
+  jump_to_lai_interface: boolean
+  load_time_ms: number
+  number_of_lots: number
+}
