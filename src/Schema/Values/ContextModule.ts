e5ae0aeffe76497d43755f55febc1d6140fbd404
Mike Hromchak <mike@artsymail.com>
2020-11-06 16:45:43 -0500

    Add context module for aboutThisAuction

--- src/Schema/Values/ContextModule.ts
+++ src/Schema/Values/ContextModule.ts
@@ -6,6 +6,7 @@
  */
 export enum ContextModule {
   aboutTheWork = "aboutTheWork",
+  aboutThisAuction = "aboutThisAuction",
   articleArtist = "articleArtist",
   artistCard = "artistCard",
   artistHeader = "artistHeader",
