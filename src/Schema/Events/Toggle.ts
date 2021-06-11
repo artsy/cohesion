258ba9765ff08e8caa10b4e61758501e246f6748
louislecl <louis.lecluse@artsymail.com>
2021-06-11 19:34:19 -0400

    saved search tracking

--- src/Schema/Events/Toggle.ts
+++ src/Schema/Events/Toggle.ts
@@ -35,3 +35,29 @@ export interface ToggledNotification {
   original: number
   subject: PushNotificationType
 }
+
+/**
+ * A user toggles a saved search on/off on iOS
+ *
+ *  This schema describes events sent to Segment from [[toggledNotification]]
+ *
+ *  @example
+ *  ```
+ *  {
+ *    action: "toggledSavedSearch",
+ *    context_screen_owner_type: "artist",
+ *    context_screen_owner_id: "58de681f275b2464fcdde097",
+ *    modified: 1,
+ *    original: 0,
+ *    saved_search_id: "58de681f275b2464fcdde097"
+ *  }
+ * ```
+ */
+export interface ToggledSavedSearch {
+  action: ActionType.toggledSavedSearch
+  context_screen_owner_type: ScreenOwnerType
+  context_screen_owner_id?: string
+  modified: number
+  original: number
+  saved_search_id: string
+}
