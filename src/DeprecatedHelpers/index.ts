5852d14ffb845dd9372a059a43e94d68e2e6412f
araujobarret <araujobarret@gmail.com>
2021-10-07 12:02:24 +0200

    refactor(FX-2978): remove authImpression deprecated helper
    Co-authored-by: Rachel Quartararo <5084923D7+rquartararo@users.noreply.github.com>

--- src/DeprecatedHelpers/index.ts
+++ src/DeprecatedHelpers/index.ts
@@ -1,6 +1,5 @@
 export * from "./AddToCalendar"
 export * from "./Share"
-export * from "./Authentication/AuthImpression"
 export * from "./Authentication/CreatedAccount"
 export * from "./Authentication/ResetYourPassword"
 export * from "./Authentication/SuccessfullyLoggedIn"
