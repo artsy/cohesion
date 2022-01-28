1090251e7767da4daee9e0abcac00c1a32c5b98f
Abhiti Prabahar <abhiti.prabahar@gmail.com>
2022-01-28 16:32:30 -0500

    Updates App submission flow events

---
+++ src/Schema/Events/UserExperienceInteractions.ts
@@ -0,0 +1,30 @@
+import { ContextModule } from "../Values/ContextModule"
+import { OwnerType } from "../Values/OwnerType"
+import { ActionType } from "."
+
+/**
+ * Schemas describing events for specific user experience design types.
+ * @packageDocumentation
+ */
+
+/**
+ * A user expands or collapses an accordion design, like in the consignments flow.
+ *
+ * This schema describes events sent to Segment from [[toggledAccordion]].
+ *
+ *  @example
+ *  ```
+ *  {
+ *    action: "toggledAccordion",
+ *    context_module: "artworkDetailsCompleted"
+ *    context_screen_owner_type: "consignmentFlow",
+ *  }
+ * ```
+ */
+export interface ToggledAccordion {
+  action: ActionType.toggledAccordion
+  context_module: ContextModule
+  context_screen_owner_type: OwnerType
+  context_screen_owner_id?: string
+  context_screen_owner_slug?: string
+}
