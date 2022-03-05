f61847a487d5d8edfa9161efd0ba56b1fa8e184a
Pavlos Vinieratos <pvinis@gmail.com>
2022-03-05 15:09:20 +0000

    Update ExperimentViewed.ts

--- src/Schema/Events/ExperimentViewed.ts
+++ src/Schema/Events/ExperimentViewed.ts
@@ -7,9 +7,9 @@ import { ActionType } from "."
  */
 
 /**
- * A user received an experiment on Artsy
+ * A user received an experiment on Artsy.
  *
- * This schema describes events sent to Segment from [[Experiment]]
+ * This schema describes events sent to Segment from [[Experiment]].
  *
  *  @example
  *  ```
@@ -23,13 +23,17 @@ import { ActionType } from "."
  *    context_owner_id: "55ed8ca57261693d930000b8",
  *  }
  * ```
+ *
+ * `payload` is used for variable values that can be sent to the clients as part of the variant.
+ * Specifically for Unleash, find some docs about `payload` here:
+ * https://docs.getunleash.io/advanced/toggle_variants#variant-payload.
  */
 export interface ExperimentViewed {
   action: ActionType.experimentViewed
   service: "unleash" | string
   experiment_name: string
-  variant_name: "control" | string // usually it will be "control" or the other names of the experiments
-  payload?: string                 // payload is used for variable values that can be sent to the clients as part of the variant
+  variant_name: "control" | string // usually it will be "control" or the other names of the variants
+  payload?: string
 
   context_owner_type: OwnerType
   context_owner_id?: string
