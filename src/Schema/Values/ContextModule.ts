8ca9d75f178f51d4fd3db1f59dd2efdc6f246bfb
dariakoko <nargle30@gmail.com>
2022-03-23 11:12:33 +0100

    added myCollectionArtworkAbout context module

--- src/Schema/Values/ContextModule.ts
+++ src/Schema/Values/ContextModule.ts
@@ -95,6 +95,7 @@ export enum ContextModule {
   myCollectionArtwork = "myCollectionArtwork",
   myCollectionAddArtworkAddArtist = "myCollectionAddArtworkAddArtist",
   myCollectionArtworkInsights = "myCollectionArtworkInsights",
+  myCollectionArtworkAbout = "myCollectionArtworkAbout",
   myCollectionComparableWorks = "myCollectionComparableWorks",
   myCollectionHome = "myCollectionHome",
   newWorksByArtistsYouFollowRail = "newWorksByArtistsYouFollowRail",
