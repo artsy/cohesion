19f91fcd0c1f7b0588a542d85c9816ab4e535279
Ole <ole.richter91@gmail.com>
2023-02-23 12:12:19 +0100

    feat: Add curatorPicksRail context module

--- src/Schema/Values/ContextModule.ts
+++ src/Schema/Values/ContextModule.ts
@@ -60,6 +60,7 @@ export enum ContextModule {
   curatedCollections = "curatedCollections",
   curatedHighlightsRail = "curatedHighlightsRail",
   curatedTrendingArtistsRail = "curatedTrendingArtistsRail",
+  curatorPicksRail = "curatorPicksRail",
   currentAuctions = "currentAuctions",
   currentShowsRail = "currentShowsRail",
   doMoreOnArtsy = "doMoreOnArtsy",
