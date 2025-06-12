44a8c548e235a78b5233c3d7dd06ed0dbbb81679
Jackie Potts <jacqueline.a.potts@gmail.com>
2025-06-12 13:21:44 -0400

    A bit of renaming and address leo's event feedback

--- src/Schema/CMS/Events/BulkEditFlow.ts
+++ src/Schema/CMS/Events/BulkEditFlow.ts
@@ -6,22 +6,124 @@
 import { CmsContextModule } from "../Values/CmsContextModule"
 
 /**
- * Generic interface for Batch Edit "click" events
+ * Partners clicked change availability pill
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "change availability",
+ *   value: "on hold", // e.g. "on hold", "available", "not for sale"
+ * }
+ * ```
  */
-export interface CmsBatchEditClickEvent {
+export interface CmsBulkEditClickedChangeAvailability {
   action: "click"
   context_module: CmsContextModule.bulkEditFlow
-  label: CmsBatchEditClickLabel
+  label: "change availability"
+  value: string
 }
 
 /**
- * All valid labels for Batch Edit "click" events
+ * Partners clicked bulk edit pill
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "bulk edit",
+ *   value: "5", // how many artworks selected
+ * }
+ * ```
  */
-export type CmsBatchEditClickLabel =
-  | "click edit"
-  | "click shortlist"
-  | "click confirm edits"
-  | "click resolve all conflicts"
+export interface CmsBulkEditClickedEditPill {
+  action: "click"
+  context_module: CmsContextModule.bulkEditFlow
+  label: "bulk edit"
+  value: string
+}
+
+/**
+ * Partners clicked shortlist pill
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "shortlist",
+ *   value: "5", // how many artworks selected
+ * }
+ * ```
+ */
+export interface CmsBulkEditClickedShortlistPill {
+  action: "click"
+  context_module: CmsContextModule.bulkEditFlow
+  label: "shortlist"
+  value: string // how many artworks selected
+}
+
+/**
+ * Partners clicked resolve all conflicts link
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "resolve all conflicts",
+ * }
+ * ```
+ */
+export interface CmsBulkEditClickedResolveAllConflicts {
+  action: "click"
+  context_module: CmsContextModule.bulkEditFlow
+  label: "resolve all conflicts"
+}
+
+/**
+ * Partners clicked confirm edits button
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "confirm edit",
+ * }
+ * ```
+ */
+export interface CmsBulkEditClickedConfirmEdits {
+  action: "click"
+  context_module: CmsContextModule.bulkEditFlow
+  label: "confirm edit"
+}
+
+/**
+ * Partners clicked cancel button in bulk edit drawer
+ *
+ * @example
+ * ```
+ * {
+ *   action: "click",
+ *   context_module: "Artworks - bulk edit",
+ *   label: "cancel",
+ * }
+ * ```
+ */
+export interface CmsBulkEditClickedCancelEdits {
+  action: "click"
+  context_module: CmsContextModule.bulkEditFlow
+  label: "cancel"
+}
+
 
 export type CmsBulkEditFlow =
-  | CmsBatchEditClickEvent
+  | CmsBulkEditClickedChangeAvailability
+  | CmsBulkEditClickedEditPill
+  | CmsBulkEditClickedShortlistPill
+  | CmsBulkEditClickedResolveAllConflicts
+  | CmsBulkEditClickedConfirmEdits
+  | CmsBulkEditClickedCancelEdits
