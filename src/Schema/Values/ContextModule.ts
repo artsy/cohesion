6528d278e01e564bb80b0309c7eeebf9ce28e2f6
xander-pero <xander.pero@artsymail.com>
2026-01-21 12:25:30 -0500

    conversation ContextModule

--- src/Schema/Values/ContextModule.ts
+++ src/Schema/Values/ContextModule.ts
@@ -88,6 +88,7 @@ export enum ContextModule {
   consignmentInquiryForm = "consignmentInquiryForm",
   consignSubmissionFlow = "consignSubmissionFlow",
   contactInformation = "contactInformation",
+  conversation = "conversation",
   conversationBuyNowConfirmArtwork = "conversationBuyNowConfirmArtwork",
   conversationMakeOfferConfirmArtwork = "conversationMakeOfferConfirmArtwork",
   createAlert = "createAlert",
