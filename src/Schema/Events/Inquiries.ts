d33df045076743afee1aa8d5e47152af842e4920
Jackie Potts <jacqueline.a.potts@gmail.com>
2026-02-11 12:15:23 -0500

    Add inquiry_checkboxes and proper labels to sentArtworkInquiry tracking event

--- src/Schema/Events/Inquiries.ts
+++ src/Schema/Events/Inquiries.ts
@@ -17,6 +17,7 @@ import { ActionType } from "."
  *    artwork_id: "582b1bc09c18db3b0c000999",
  *    artwork_slug: "philippe-halsman-alfred-hitchcock-during-the-filming-of-the-birds",
  *    inquiry_id: "614e3063ef0b81000bc4f45a",
+ *    inquiry_checkboxes: ["shipping_quote", "price_and_availability"]
  *    products: [
         {
           price: 6000,
@@ -33,6 +34,7 @@ export interface SentArtworkInquiry {
   artwork_id: string
   artwork_slug: string
   inquiry_id: string
+  inquiry_checkboxes?: InquiryCheckboxLabel[]
   products: InquiryProduct[]
 }
 
@@ -43,4 +45,13 @@ export type InquiryProduct = {
   price: string
   product_id: string
   quantity: 1
+}
+
+export enum InquiryCheckboxLabel {
+  Shipping = "shipping_quote",
+  PriceAndAvailability = "price_and_availability",
+  ConditionAndProvenance = "condition_and_provenance",
+  SimilarWork = "similar_work",
+  ArtistInformation = "artist_information",
+  ArtworkInformation = "artwork_information",
 }
