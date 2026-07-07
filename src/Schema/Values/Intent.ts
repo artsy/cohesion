cf2ea62a232cc17cee982c156f553830b7131463
xander-pero <xander.pero@artsymail.com>
2026-07-07 10:48:09 -0400

    Update Intent.ts

--- src/Schema/Values/Intent.ts
+++ src/Schema/Values/Intent.ts
@@ -19,6 +19,7 @@ export enum Intent {
   seePriceAuctionRecords = "seePriceAuctionRecords",
   seeRealizedPriceAuctionRecords = "seeRealizedPriceAuctionRecords",
   makeOffer = "makeOffer",
+  oneTap = "oneTap",
   registerToBid = "registerToBid",
   requestConditionReport = "requestConditionReport",
   saveArtwork = "saveArtwork",
@@ -50,6 +51,7 @@ export type AuthIntent =
   | Intent.seePriceAuctionRecords
   | Intent.seeRealizedPriceAuctionRecords
   | Intent.makeOffer
+  | Intent.oneTap
   | Intent.registerToBid
   | Intent.requestConditionReport
   | Intent.saveArtwork
